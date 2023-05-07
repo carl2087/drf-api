221d8160b23dfaba841fb19ba0cedfd4fff38b1c
Carl <carlelli5@hotmail.co.uk>
2023-05-07 15:55:01 +0000

    added coce to get like id and following id and return it to the user

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,18 +1,41 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+# Now, I can define ‘following_id’, a new method  field. I’ll also add it
+# to the fields list.
+# Next, the get_following_id method.
+# Inside, I’ll get the current user from the context  object and check
+# if the user is authenticated.
+# Then I will check if the logged in user  is following any of the other
+# profiles.
+# To do this I will filter the Follower object.
+# If the logged in user is following this  profile an instance
+# will be returned.
+# If the logged in user is not following  this profile, None will be returned.
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'following_id'
         ]
